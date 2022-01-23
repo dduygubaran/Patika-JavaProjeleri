438e79eaa98a41bc94aa9c3a3f14f2fbd49bd32b
Duygu Baran <83429611+dduygubaran@users.noreply.github.com>
2022-01-23 14:31:10 +0300

    Update TaksimetreProgramı.java

--- src/TaksimetreProgramı.java
+++ src/TaksimetreProgramı.java
@@ -1,5 +1,12 @@
 import java.util.Scanner;
 
+/* Taksimetre Programı
+Java ile gidilen mesafeye (KM) göre taksimetre tutarını ekrana yazdıran programı yazın.
+
+*Taksimetre KM başına 2.20 TL tutmaktadır.
+*Minimum ödenecek tutar 20 TL'dir. 20 TL altında ki ücretlerde yine 20 TL alınacaktır.
+*Taksimetre açılış ücreti 10 TL'dir.*/
+
 public class TaksimetreProgramı {
 
     public static void main(String[] args) {
